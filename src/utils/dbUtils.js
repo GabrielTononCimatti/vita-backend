c1240bf7f06ac8728bae0bd80b6fc49a7cee95e2
GabrielTononCimatti <gabrieltonci@hotmail.com>
2025-08-18 22:43:56 -0300

    Adição de features tokenToId, docToObject e snapshotToArray para a conversão de dados. Função getUserData realocada para userModel para a melhor organização.

--- src/utils/dbUtils.js
+++ src/utils/dbUtils.js
@@ -1,26 +1,42 @@
 import {auth, db} from "../config/firebaseConfig.js";
 
-export const getUserData = async (idToken) =>
+export const tokenToId = (token) =>
 {
+    let firebase_uid, resultado;
+
+    if(!token)
+        throw new Error('Unauthorized. Nenhum token fornecido.', {cause: {status: 401}});
+
     try
     {
-        const firebase_uid = (await auth.verifyIdToken(idToken)).uid;
-        const resultado = await db.collection('usuarios').where('firebase_uid', '==', firebase_uid).limit(1).get();
-
-        if (resultado.empty) {
-            throw new Error('Usuário não encontrado na base de dados.');
-        }
-        let userData;
-        resultado.forEach(doc =>
-        {
-            userData = { id: doc.id, ...doc.data() };
-        });
-
-        return userData;
+        firebase_uid = (await auth.verifyIdToken(idToken)).uid;
+        resultado = await db.collection('usuarios').where('firebase_uid', '==', firebase_uid).limit(1).get();
     }
-    catch (error)
+    catch(error)
     {
         throw error;
     }
+
+    if (resultado.empty)
+        throw new Error('Usuário não encontrado na base de dados.', {cause: {status: 404}});
+
+    return resultado.docs[0].id;
+}
+
+
+export const docToObject = (doc) =>
+{
+    if (!doc.exists) {
+        return null;
+    }
+    return {id: doc.id, ...doc.data()};
 };
 
+export const snapshotToArray = (snapshot) =>
+{
+    const results = [];
+    snapshot.forEach(doc => {
+        results.push(docToObject(doc));
+    });
+    return results;
+};
